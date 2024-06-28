436e413b9bf7c7606d9fd3b3f532bd80c75eb430
mhorky <mhorky@redhat.com>
2024-06-28 17:49:38 +0200

    feat: Support excluding non-nested keys from the playbook

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module com.github/m-horky/playbook-verifier
 go 1.22.3
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,167 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 
-	"gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v2"
 )
 
-var VerificationError = errors.New("verification failed")
+var DynamicLabels = map[string]any{"hosts": nil, "vars": nil}
 
-// FIXME We'll need to use stable map that keeps the insertion order.
-// E.g. https://pkg.go.dev/github.com/wk8/go-ordered-map.
+type PlaybookError struct {
+	message string
+}
 
-type PlaybookType = []map[string]any
+func (e PlaybookError) Error() string {
+	return fmt.Sprintf("playbook error: %s", e.message)
+}
+
+type VerificationError struct {
+	message string
+}
+
+func (e VerificationError) Error() string {
+	return fmt.Sprintf("verification error: %s", e.message)
+}
 
 type PlaybookSource struct {
 	stdin bool
 	path  string
+}
+
+// DirtyPlaybook object can be used to manipulate and (un)marshal its data.
+type DirtyPlaybook yaml.MapSlice
+
+// CleanPlaybook does not contain dynamic elements.
+type CleanPlaybook yaml.MapSlice
+
+func UnmarshalPlaybook(playbook []byte) (DirtyPlaybook, error) {
+	var data []DirtyPlaybook
+	if err := yaml.Unmarshal(playbook, &data); err != nil {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return nil, PlaybookError{"playbook contains no data"}
+	}
+	if len(data) > 1 {
+		return nil, PlaybookError{"input cannot contain more than one playbook"}
+	}
+	return data[0], nil
+}
+
+// GetExclusions extracts dynamic keys that are meant to be excluded from the playbook hash.
+func (p *DirtyPlaybook) GetExclusions() ([][]string, error) {
+	rawExclusions := ""
+	for _, item := range *p {
+		if item.Key.(string) != "vars" {
+			continue
+		}
+		vars := item.Value.(DirtyPlaybook)
+		for _, pair := range vars {
+			if pair.Key.(string) != "insights_signature_exclude" {
+				continue
+			}
+			rawExclusions = pair.Value.(string)
+			break
+		}
+	}
+
+	if rawExclusions == "" {
+		return nil, PlaybookError{"playbook doesn't contain key 'insights_signature_exclude'"}
+	}
+
+	var exclusions [][]string
+	for _, exclusion := range strings.Split(rawExclusions, ",") {
+		exclusionBits := strings.TrimPrefix(exclusion, "/")
+		exclusions = append(exclusions, strings.Split(exclusionBits, "/"))
+	}
+	return exclusions, nil
+}
+
+func (p *DirtyPlaybook) Clean() (*CleanPlaybook, error) {
+	exclusions, err := p.GetExclusions()
+	if err != nil {
+		return nil, err
+	}
+
+	clean := CleanPlaybook{}
+	for _, directValue := range *p {
+		directValueName := directValue.Key.(string)
+		skipDirectValue := false
+
+		// exclusion of top-level/direct values
+		for _, exclusion := range exclusions {
+			if directValueName == exclusion[0] && len(exclusion) == 1 {
+				skipDirectValue = true
+			}
+		}
+		if skipDirectValue {
+			slog.Debug("excluding", slog.String("path", directValue.Key.(string)))
+			continue
+		}
+
+		// exclusion of nested values
+		// TODO
+
+		clean = append(clean, directValue)
+	}
+	return &clean, nil
+}
+
+// Marshall takes in the playbook and marshals it into a string
+// as per the requirements of the hashing scheme.
+func (p *CleanPlaybook) Marshall() ([]byte, error) {
+	// TODO dict as ...
+	// TODO list as ...
+	// TODO ...
+
+	return []byte{}, nil
+}
+
+// cleanPlaybook deletes specific keys from the playbook.
+//
+// Some parts of the Ansible playbook are dynamic (e.g. hosts, the signature) and cannot be
+// signed, because the signature would be unique for every playbook that is generated.
+//
+// This function removes these variable sections.
+func cleanPlaybook(p *DirtyPlaybook) (*CleanPlaybook, error) {
+	playbook := CleanPlaybook{}
+	for _, v := range *p {
+		playbook = append(playbook, yaml.MapItem{Key: v.Key, Value: v.Value})
+	}
+
+	for _, exclusion := range []string{} {
+		var elements []string
+		for _, element := range strings.Split(exclusion, "/") {
+			if len(element) == 0 {
+				continue
+			}
+			elements = append(elements, element)
+		}
+
+		// elements[0] has to be key explicitly allowed to be excluded
+		if _, ok := DynamicLabels[elements[0]]; !ok {
+			return nil, VerificationError{
+				fmt.Sprintf("key '%s' is not allowed to be excluded", exclusion),
+			}
+		}
+
+		// simple key deletion
+		if len(elements) == 1 {
+			slog.Debug("deleted dynamic key", "key", exclusion)
+		}
+		// nested key deletion
+		if len(elements) == 2 {
+			slog.Debug("deleted dynamic key", "key", exclusion)
+		}
+	}
+
+	return &playbook, nil
 }
 
 // NewPlaybookSource detects the location for the playbook.
@@ -47,51 +189,55 @@ func main() {
 
 	// Load playbook from stdin
 	source := NewPlaybookSource()
-	rawPlaybook, err := getPlaybookContent(source)
+	rawPlaybook, err := readPlaybook(source)
 	if err != nil {
-		slog.Error("error getting playbook content:", slog.Any("error", err))
+		slog.Error("error getting playbook content", slog.Any("error", err))
 		return
 	}
 
-	playbook, err := parsePlaybook(rawPlaybook)
+	// Convert it into object
+	dirty, err := UnmarshalPlaybook(rawPlaybook)
 	if err != nil {
-		slog.Error("could not parse playbook:", slog.Any("error", err))
+		slog.Error("could not parse playbook", slog.Any("error", err))
 		return
 	}
 
-	for k, v := range playbook[0] {
-		fmt.Println(k, v)
+	// Delete dynamic elements
+	clean, err := dirty.Clean()
+	if err != nil {
+		slog.Error("could not clean playbook", slog.Any("error", err))
 	}
+	fmt.Println("clean", clean)
 
-	// Verify its signature
-	// Emit playbook to stdout
+	// Serialize it
+
+	// Create a hash
+
+	// Verify the hash
+
+	// Print the original playbook
+	return
 }
 
-// getPlaybookContent reads the playbook verifier from either stdin or from a file.
-func getPlaybookContent(source PlaybookSource) ([]byte, error) {
+// readPlaybook reads the playbook verifier from either stdin or from a file.
+func readPlaybook(source PlaybookSource) ([]byte, error) {
 	var rawPlaybook []byte
 	if source.stdin {
-		readPlaybook, err := io.ReadAll(os.Stdin)
+		playbook, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			slog.Error("could not read playbook from stdin", slog.Any("error", err))
 			return []byte{}, err
 		}
-		rawPlaybook = readPlaybook
+		rawPlaybook = playbook
 	} else {
-		readPlaybook, err := os.ReadFile(source.path)
+		playbook, err := os.ReadFile(source.path)
 		if err != nil {
 			slog.Error("could not read playbook from file", slog.Any("error", err))
 			return []byte{}, err
 		}
-		rawPlaybook = readPlaybook
+		rawPlaybook = playbook
 	}
 
 	slog.Debug("playbook obtained", slog.Int("bytes", len(rawPlaybook)))
 	return rawPlaybook, nil
-}
-
-func parsePlaybook(playbook []byte) (PlaybookType, error) {
-	data := PlaybookType{}
-	err := yaml.Unmarshal(playbook, &data)
-	return data, err
 }
